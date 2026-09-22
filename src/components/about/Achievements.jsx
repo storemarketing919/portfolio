7fe898892fa09c549b1d8c5452a005c2baaac36e
Marvin Carino <storemarketing919@gmail.com>
2026-09-21 19:04:11 -0700

    fix error in achievemnets

--- src/components/about/Achievements.jsx
+++ src/components/about/Achievements.jsx
@@ -1,11 +1,5 @@
 import React from "react";
-
-const achievementsContent = [
-  { title: "12", subTitle1: "years of", subTitle2: "experience" },
-  { title: "97", subTitle1: "completed", subTitle2: "projects" },
-  { title: "81", subTitle1: "Happy", subTitle2: "customers" },
-  { title: "53", subTitle1: "awards", subTitle2: "won" },
-];
+import achievementsContent from "../../data/AchievementsContent.jsx";
 
 const Achievements = () => {
   return (
