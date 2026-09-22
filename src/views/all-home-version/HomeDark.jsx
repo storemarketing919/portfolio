4a7360c8c28a54d1b0d0033b49d3c4b19e064896
Marvin Carino <storemarketing919@gmail.com>
2026-09-21 17:08:13 -0700

    change main background image

--- src/views/all-home-version/HomeDark.jsx
+++ src/views/all-home-version/HomeDark.jsx
@@ -44,8 +44,7 @@ const HomeDark = () => {
           <TabPanel
             className="home "
             style={{
-              backgroundImage:
-                "radial-gradient(circle at 80% 20%, rgba(76, 92, 220, 0.42), transparent 34%), linear-gradient(135deg, #020817 0%, #071d45 52%, #10052b 100%)",
+              backgroundImage: "url('/img/hero/background.png')",
               backgroundSize: "cover",
               backgroundPosition: "center",
               backgroundRepeat: "no-repeat",
